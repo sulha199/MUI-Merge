ba49e555ca381c2fae42a52ad101893b99f5dd9c
sulha199 <sulha199@gmail.com>
2023-07-24 16:18:09 +0700

    include dist in git

---
+++ dist/main.js
@@ -0,0 +1,38 @@
+var $gXNCa$react = require("react");
+var $gXNCa$reactdomclient = require("react-dom/client");
+var $gXNCa$muimaterial = require("@mui/material");
+
+function $parcel$interopDefault(a) {
+  return a && a.__esModule ? a.default : a;
+}
+
+
+
+
+function $da11a1101b2a894a$export$2e2bcd8739ae039() {
+    return /*#__PURE__*/ (0, ($parcel$interopDefault($gXNCa$react))).createElement((0, $gXNCa$muimaterial.Box), {
+        sx: {
+            width: "100%",
+            maxWidth: 500,
+            margin: "0 auto"
+        }
+    }, /*#__PURE__*/ (0, ($parcel$interopDefault($gXNCa$react))).createElement((0, $gXNCa$muimaterial.Typography), {
+        variant: "h1",
+        gutterBottom: true
+    }, "Hello MUI!"), /*#__PURE__*/ (0, ($parcel$interopDefault($gXNCa$react))).createElement((0, $gXNCa$muimaterial.Stack), {
+        spacing: 2,
+        direction: "row"
+    }, /*#__PURE__*/ (0, ($parcel$interopDefault($gXNCa$react))).createElement((0, $gXNCa$muimaterial.Button), {
+        variant: "contained"
+    }, "Contained"), /*#__PURE__*/ (0, ($parcel$interopDefault($gXNCa$react))).createElement((0, $gXNCa$muimaterial.Button), {
+        variant: "outlined"
+    }, "Outlined")));
+}
+
+
+const $4fa36e821943b400$var$container = document.getElementById("app");
+const $4fa36e821943b400$var$root = (0, $gXNCa$reactdomclient.createRoot)($4fa36e821943b400$var$container);
+$4fa36e821943b400$var$root.render(/*#__PURE__*/ (0, ($parcel$interopDefault($gXNCa$react))).createElement((0, $da11a1101b2a894a$export$2e2bcd8739ae039), null));
+
+
+//# sourceMappingURL=main.js.map
---
+++ dist/module.js
@@ -0,0 +1,35 @@
+import $5OpyM$react from "react";
+import {createRoot as $5OpyM$createRoot} from "react-dom/client";
+import {Box as $5OpyM$Box, Typography as $5OpyM$Typography, Stack as $5OpyM$Stack, Button as $5OpyM$Button} from "@mui/material";
+
+
+
+
+
+function $226872678f9f0005$export$2e2bcd8739ae039() {
+    return /*#__PURE__*/ (0, $5OpyM$react).createElement((0, $5OpyM$Box), {
+        sx: {
+            width: "100%",
+            maxWidth: 500,
+            margin: "0 auto"
+        }
+    }, /*#__PURE__*/ (0, $5OpyM$react).createElement((0, $5OpyM$Typography), {
+        variant: "h1",
+        gutterBottom: true
+    }, "Hello MUI!"), /*#__PURE__*/ (0, $5OpyM$react).createElement((0, $5OpyM$Stack), {
+        spacing: 2,
+        direction: "row"
+    }, /*#__PURE__*/ (0, $5OpyM$react).createElement((0, $5OpyM$Button), {
+        variant: "contained"
+    }, "Contained"), /*#__PURE__*/ (0, $5OpyM$react).createElement((0, $5OpyM$Button), {
+        variant: "outlined"
+    }, "Outlined")));
+}
+
+
+const $cf838c15c8b009ba$var$container = document.getElementById("app");
+const $cf838c15c8b009ba$var$root = (0, $5OpyM$createRoot)($cf838c15c8b009ba$var$container);
+$cf838c15c8b009ba$var$root.render(/*#__PURE__*/ (0, $5OpyM$react).createElement((0, $226872678f9f0005$export$2e2bcd8739ae039), null));
+
+
+//# sourceMappingURL=module.js.map
